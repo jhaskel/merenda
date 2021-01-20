af442b98fe0654bd43a138cdac61a35cb5d0bf80
Joao Haskel <johaskel@gmail.com>
2021-01-20 17:09:56 -0300

    produto - estoque

--- src/main/java/com/doisbitsw/licencas/api/produtos/ProdutoRepository.java
+++ src/main/java/com/doisbitsw/licencas/api/produtos/ProdutoRepository.java
@@ -1,10 +1,12 @@
 package com.doisbitsw.licencas.api.produtos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
+    @Query(value = "select * from produto  order by id desc where ativo = true", nativeQuery = true)
     List<Produto> findByCode(String code);
 }
