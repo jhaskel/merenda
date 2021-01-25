5b80752b7c51ab977b57d9b4f3162567ffc628e0
Joao Haskel <johaskel@gmail.com>
2021-01-25 10:53:17 -0300

    pedidos

--- src/main/java/com/doisbitsw/licencas/api/pedidoItens/PedidoItensRepository.java
+++ src/main/java/com/doisbitsw/licencas/api/pedidoItens/PedidoItensRepository.java
@@ -8,6 +8,9 @@ import java.util.List;
 
 public interface PedidoItensRepository extends JpaRepository<PedidoItens, Long> {
 
+    @Query(value = "SELECT *  FROM pedido_itens WHERE  isaf = false;", nativeQuery = true)
+    List<PedidoItens> findAll();
+
 
     @Query(value = "SELECT *  FROM pedido_itens WHERE pedido = :pedido and ischeck = false;", nativeQuery = true)
     List<PedidoItens> findByPedido(String pedido);
