a8ee075dc9f5505d9fae625a22e50e3b2d4aaf1f
Joao Haskel <johaskel@gmail.com>
2021-02-16 10:53:09 -0300

    afPedido

--- src/main/java/com/doisbitsw/licencas/afPedido/AfPedidoRepository.java
+++ src/main/java/com/doisbitsw/licencas/afPedido/AfPedidoRepository.java
@@ -7,16 +7,15 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface AfPedidoRepository extends JpaRepository<AfPedido, Long> {
-    @Query(value = "SELECT * FROM af WHERE ativo = TRUE ORDER BY id desc", nativeQuery = true)
 
+    @Query(value = "SELECT * FROM af_pedido  ORDER BY id desc", nativeQuery = true)
     List<AfPedido> findAll();
 
     @Query(value = "SELECT * FROM af_pedido\n" +
             " WHERE af = :af ", nativeQuery = true)
     List<AfPedido> findByAf(Long af);
 
-    @Query(value = "SELECT COUNT(id) as total FROM af WHERE ativo = TRUE AND isautorizado = false", nativeQuery = true)
-    long findAf();
+
 
 
 }
